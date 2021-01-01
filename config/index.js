f42b484071786fe405780dad81328c5b360a20b1
betrox <betrox@qq.com>
2021-01-02 00:45:04 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 module.exports = {
   username: process.env.TEXTNOW_USERNAME || "",
   password: process.env.TEXTNOW_PASSWORD || "",
-  recipient: process.env.TEXTNOW_RECIPIENT || "(726) 666-0002",
+  recipient: process.env.TEXTNOW_RECIPIENT || "(912) 867-0285",
   message: process.env.TEXTNOW_MESSAGE || "autosend message",
 };
